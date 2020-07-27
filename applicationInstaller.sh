fb5bfa8d330dc5d57688033e9d2938c596dd2a7e
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-07-28 00:00:58 +0900

    add: Apps を brew, cask, mas によりインストールするスクリプトを追加

---
+++ applicationInstaller.sh
@@ -0,0 +1,63 @@
+# -----------------------------
+# homebrew
+
+# brew install gnuplot --with-aquaterm --with-x11
+# brew install gnuplot
+# brew install gcc
+brew install tmux
+brew install reattach-to-user-namespace # anti [exited]
+brew install git
+brew install vim
+# brew install nkf
+# brew install imagemagick
+brew install carthage
+brew install mint
+brew install xcodegen
+brew install yabai
+brew install skhd
+brew install spacebar
+
+# -----------------------------
+# homebrew cask
+
+# brew cask install aquaterm
+# brew cask install xquartz
+# brew cask install mactex
+# brew cask install mendeley
+# brew cask install emacs
+# brew cask install atom
+brew cask install google-japanese-ime
+brew cask install google-chrome
+brew cask install font-source-code-pro
+# brew cask install firefox
+# brew cask install google-backup-and-sync
+# brew cask install dropbox
+# brew cask install macs-fan-control
+# brew cask install scroll-reverser
+# brew cask install ccleaner
+brew cask install alfred
+# brew cask install cheatsheet
+brew cask install karabiner
+# brew cask install gimp
+# brew cask install inkscape
+brew cask install 
+
+# -----------------------------
+# mas
+# * sign-in to your AppleID first
+
+mas install 497799835    # Xcode
+# mas install 414298354    # ToyViewer
+mas install 539883307    # LINE
+mas install 803453959    # Slack
+# mas install 1024640650   # cotEditor
+mas install 409183694    # Keynote
+# mas install 409203825    # Numbers
+mas install 409201541    # Pages
+mas install 425424353    # The Unarchiver
+# mas install 417375580    # BetterSnapTool
+# mas install 405399194    # Kindle
+# mas install 410628904    # Wunderlist
+# mas install 406056744    # Evernote
+# mas install 1349670778   # Mathpix
+
