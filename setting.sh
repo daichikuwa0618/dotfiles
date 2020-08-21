3df3dc08abc1a25819620e420be91c43a3036711
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-08-21 12:12:47 +0900

    add: discord, quicklook casks

--- setting.sh
+++ setting.sh
@@ -144,6 +144,18 @@ brew cask install obs
 brew cask install obs-virtualcam
 brew cask install zoomus
 brew cask install bettertouchtool 
+brew cask install discord
+
+# Quicklook settings
+brew cask install qlcolorcode
+brew cask install qlstephen
+brew cask install qlmarkdown
+brew cask install quicklook-json
+brew cask install qlprettypatch
+brew cask install quicklook-csv
+brew cask install qlimagesize
+brew cask install webpquicklook
+brew cask install suspicious-package
 
 # install font by cask
 brew tap homebrew/cask-fonts
