591956a91801990bac41da099e0d2168fcb2aec7
Daichi Hayashi <daichikuwa0618@gmail.com>
2021-08-03 23:09:07 +0900

    PlugInstall を叩かないようにコメントアウト

--- vim.sh
+++ vim.sh
@@ -4,4 +4,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
          https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-vim -c ":PlugInstall"
+# vim -c ":PlugInstall"
+
