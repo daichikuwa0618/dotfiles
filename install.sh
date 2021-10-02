ed7bb7e7cad1ae29f911b191dbbaa0ad4cc5631f
Daichi Hayashi <daichikuwa0618@gmail.com>
2021-10-02 17:43:42 +0900

    install.sh で xvim を実行

--- install.sh
+++ install.sh
@@ -9,9 +9,14 @@ cd $DOTPATH
 echo "start zsh"
 sh $DOTPATH/zsh/install.sh
 echo "end zsh"
+
 echo "start vim"
 sh $DOTPATH/vim/install.sh
 echo "end vim"
+
+echo "start xvim"
+sh $DOTPATH/xvim/install.sh
+echo "end xvim"
 
 # global gitignore
 ln -sf "$DOTPATH"/gitignore_global "$HOME"/.config/git/ignore
