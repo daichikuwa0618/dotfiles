0b6490275a4167356f57412b2991ddcb1f4d0d89
Daichi Hayashi <daichikuwa0618@gmail.com>
2021-10-02 15:40:22 +0900

    zsh dir を作ってそこにまとめて、 zsh 用 alias スクリプトを作成

---
+++ zsh/install.sh
@@ -0,0 +1,9 @@
+#! /bin/zsh
+
+DOTPATH=$HOME/dotfiles/zsh
+
+ln -sf $DOTPATH/zpreztorc $HOME/.zpreztorc
+ln -sf $DOTPATH/zshrc $HOME/.zshrc
+ln -sf $DOTPATH/zprofile $HOME/.zprofile
+ln -sf $DOTPATH/secret.zshrc $HOME/secret.zshrc
+
