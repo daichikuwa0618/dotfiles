5aa35d450b3f43bcba2a4dd9f2a78cad4994fffd
Daichi Hayashi <daichikuwa0618@gmail.com>
2021-10-02 17:41:31 +0900

    install.sh をサブディレクトリの install.sh を実行するだけに修正

--- install.sh
+++ install.sh
@@ -1,29 +1,18 @@
 #! /bin/sh
 
-DOTPATH=~/dotfiles/dotfiles
+DOTPATH=$HOME/dotfiles
 
 echo "\$DOTPATH : $DOTPATH"
 echo "Created dotfile symbolic links."
-cd dotfiles
-for f in .??*
-do
-  # Exclude files
-  if [ "$f" = ".gitmodules" ] || [ "$f" = ".git" ] || [ "$f" = ".gitconfig" ] || [ "$f" = ".zshrc_legacy" ] || [ "$f" = ".bash_profile" ] || [ "$f" = ".bashrc" ];then
-    continue
-  fi
+cd $DOTPATH
 
-  ln -sf "$DOTPATH"/"$f" "$HOME"/"$f"
-  if [ $? -eq 0 ]; then
-    printf "    %-25s -> %s\n" "\$DOTPATH/$f" "\$HOME/$f"
-  fi
-done
+echo "start zsh"
+sh $DOTPATH/zsh/install.sh
+echo "end zsh"
+echo "start vim"
+sh $DOTPATH/vim/install.sh
+echo "end vim"
 
-# NeoVim の設定ファイルと Vim の config ファイルを統一する
-ln -sf "$DOTPATH"/.vimrc "$HOME"/.config/nvim/init.vim
-printf "    %-25s -> %s\n" "\$DOTPATH/.vimrc" "\$HOME/.config/nvim/init.vim"
-# secret files
-ln -sf "$DOTPATH"/secret.zshrc "$HOME"/secret.zshrc
-printf "    %-25s -> %s\n" "\$DOTPATH/secret.zshrc" "\$HOME/secret.zshrc"
 # global gitignore
 ln -sf "$DOTPATH"/gitignore_global "$HOME"/.config/git/ignore
 printf "    %-25s -> %s\n" "\$DOTPATH/gitignore_global" "\$HOME/.config/git/ignore"
