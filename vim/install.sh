18c9cc5f418d91734bde1c5925a17df148583a71
Daichi Hayashi <daichikuwa0618@gmail.com>
2022-09-25 21:20:15 +0900

    Add Coc-nvim config

--- vim/install.sh
+++ vim/install.sh
@@ -5,3 +5,5 @@ ln -sf $DOTPATH/vim/vimrc $HOME/.config/nvim/init.vim
 # for Vim
 ln -sf $DOTPATH/vim/vimrc $HOME/.vimrc
 
+# coc.nvim
+ln -sf $DOTPATH/vim/config/plugin-config/coc-settings.json $HOME/.config/nvim/coc-settings.json
