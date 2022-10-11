23cfaed32723679c51898afa01f809a0ac802b99
Daichi Hayashi <daichikuwa0618@gmail.com>
2022-10-11 18:40:22 +0900

    Modify vim install.sh to use AstroNvim

--- vim/install.sh
+++ vim/install.sh
@@ -1,9 +1,14 @@
 #! /bin/zsh
 
+# AstroNvim を使うので通常の vimrc の symlink はコメントアウトしておく
+# AstroNvim を辞めたくなったら戻せば OK
+ln -sf $DOTPATH/vim/astronvim_config.init.lua $HOME/.config/nvim/lua/user/init.lua
+
+
 # for NeoVim
-ln -sf $DOTPATH/vim/vimrc $HOME/.config/nvim/init.vim
+# ln -sf $DOTPATH/vim/vimrc $HOME/.config/nvim/init.vim
 # for Vim
-ln -sf $DOTPATH/vim/vimrc $HOME/.vimrc
+# ln -sf $DOTPATH/vim/vimrc $HOME/.vimrc
 
 # coc.nvim
-ln -sf $DOTPATH/vim/config/plugin-config/coc-settings.json $HOME/.config/nvim/coc-settings.json
+# ln -sf $DOTPATH/vim/config/plugin-config/coc-settings.json $HOME/.config/nvim/coc-settings.json
