ec40a8542ea2a670a522d1e79e149438bca62a15
Daichi Hayashi <daichikuwa0618@gmail.com>
2022-10-29 09:31:27 +0900

    Add lazygit as TUI, delta as git pager

--- install.sh
+++ install.sh
@@ -25,5 +25,12 @@ echo "end zsh"
 # global gitignore
 ln -sf "$DOTPATH"/gitignore_global "$HOME"/.config/git/ignore
 printf "    %-25s -> %s\n" "\$DOTPATH/gitignore_global" "\$HOME/.config/git/ignore"
+ln -sf "$DOTPATH"/gitconfig "$HOME"/.gitconfig
+printf "    %-25s -> %s\n" "\$DOTPATH/gitconfig" "\$HOME/.gitconfig"
+
+# lazygit の置き場を変更
+lazygit -ucd ~/.config/lazygit
+ln -sf "$DOTPATH"/lazygit.yml "$HOME"/.config/lazygit/config.yml
+printf "    %-25s -> %s\n" "\$DOTPATH/lazygit.yml" "\$HOME/.config/lazygit/config.yml"
 
 echo ""
--- zsh/alias.zshrc
+++ zsh/alias.zshrc
@@ -16,4 +16,5 @@ alias arm="exec arch -arch arm64e /bin/zsh --login"
 alias intel="exec arch -arch x86_64 /bin/zsh --login"
 alias xcode="xed ."
 alias zettel-dir="cd ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/obsidian-vault"
+alias lg="lazygit"
 
